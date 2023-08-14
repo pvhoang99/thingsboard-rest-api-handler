02ad7a852d48bbb48d11d6b5a6835e9bf2b8926b
HoangPV2 <HoangPV2@vetc.com.vn>
2023-08-14 15:51:14 +0700

    change port

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def handle(deviceName):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 6000))
     app.run(debug=True, host='0.0.0.0', port=port)
