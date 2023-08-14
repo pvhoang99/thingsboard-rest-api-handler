22f0128eeb35249ea2e07b5de9fa19b37b2d2bfc
HoangPV2 <HoangPV2@vetc.com.vn>
2023-08-14 15:40:37 +0700

    init

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, jsonify
 import subprocess
+import os
 
 app = Flask(__name__)
 
 
-@app.get("/api/<deviceName>")
+@app.post("/api/<deviceName>")
 def handle(deviceName):
     print(deviceName)
     params = [str(deviceName)]
-    subprocess.run(['bash', '/home/pham_hoang/Desktop/python/hello.sh'] + params)
+    subprocess.run(['bash', 'hello.sh'] + params)
 
     return jsonify({
         "message": "ok"
     })
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
