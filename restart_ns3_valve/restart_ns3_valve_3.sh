a0fa80263d834779299611833cbe134afc1f79a3
HoangPV2 <HoangPV2@vetc.com.vn>
2023-08-15 09:01:04 +0700

    update

--- restart_ns3_valve/restart_ns3_valve_3.sh
+++ restart_ns3_valve/restart_ns3_valve_3.sh
@@ -1,2 +1,2 @@
-docker-compose -f /usr/local/bin/docker-compose-ns3-valve down tb-gateway-ns3-valve-3 & wait
-docker-compose -f /usr/local/bin/docker-compose-ns3-valve up -d tb-gateway-ns3-valve-3 & wait
+/usr/local/bin/docker-compose -f /root/thingsboard-gateway-final/docker/docker-compose-ns3-valve down tb-gateway-ns3-valve-3 & wait
+/usr/local/bin/docker-compose -f /root/thingsboard-gateway-final/docker/docker-compose-ns3-valve up -d tb-gateway-ns3-valve-3 & wait
