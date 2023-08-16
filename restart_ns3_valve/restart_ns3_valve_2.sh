ac0f21c9369f1a6eba2a75e775151b306a3cd966
HoangPV2 <HoangPV2@vetc.com.vn>
2023-08-16 09:44:17 +0700

    update

--- restart_ns3_valve/restart_ns3_valve_2.sh
+++ restart_ns3_valve/restart_ns3_valve_2.sh
@@ -1,2 +1,4 @@
 /usr/local/bin/docker-compose -f /root/thingsboard-gateway-final/docker/docker-compose-ns3-valve.yml down tb-gateway-ns3-valve-2 & wait
 /usr/local/bin/docker-compose -f /root/thingsboard-gateway-final/docker/docker-compose-ns3-valve.yml up -d tb-gateway-ns3-valve-2 & wait
+
+echo "$(date +%c) restart ns3 valve 2 done" >> /root/restart_log/restart_log.log
